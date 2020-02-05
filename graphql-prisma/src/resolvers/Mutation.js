ac7a7743964169b4c61385dd3769c69c49a16ada
JonasMartins <jonas.w.martins@gmail.com>
2020-02-05 18:04:16 -0300

    Auto generate schema via graphql-cli and integration with node
    	modified:   .gitignore
    	new file:   graphql-prisma/.babelrc
    	new file:   graphql-prisma/.graphqlconfig
    	new file:   graphql-prisma/package.json
    	modified:   graphql-prisma/prisma/datamodel.graphql
    	new file:   graphql-prisma/src/db.js
    	new file:   graphql-prisma/src/generated/prisma.graphql
    	new file:   graphql-prisma/src/index.js
    	new file:   graphql-prisma/src/prisma.js
    	new file:   graphql-prisma/src/resolvers/Comment.js
    	new file:   graphql-prisma/src/resolvers/Mutation.js
    	new file:   graphql-prisma/src/resolvers/Post.js
    	new file:   graphql-prisma/src/resolvers/Query.js
    	new file:   graphql-prisma/src/resolvers/Subscription.js
    	new file:   graphql-prisma/src/resolvers/User.js
    	new file:   graphql-prisma/src/schema.graphql
    	new file:   graphql-prisma/yarn.lock

---
+++ graphql-prisma/src/resolvers/Mutation.js
@@ -0,0 +1,172 @@
+import uuidv4 from 'uuid/v4';
+
+const Mutation = {
+	createUser(parent, args, { db }, info) {
+		const emailTaken = users.some(user => user.email === args.data.email);
+		if (emailTaken) {
+			throw new Error('Email already taken');
+		}
+
+		const user = {
+			id: uuidv4(),
+			...args.data
+		};
+		db.users.push(user);
+		return user;
+	},
+	deleteUser(parent, args, { db }, info) {
+		const userIndex = db.users.findIndex(user => user.id === args.id);
+
+		if (userIndex === -1) {
+			throw new Error('User not found');
+		}
+		const deletedUser = db.users.splice(userIndex, 1);
+
+		posts = db.posts.filter(post => {
+			const match = post.author === args.id;
+			if (match) {
+				db.comments = db.comments.filter(comment => comment.post !== post.id);
+			}
+			return !match;
+		});
+		db.comments = db.comments.filter(comment => comment.author !== args.id);
+
+		return deletedUser[0];
+	},
+	updateUser(parent, args, { db }, info) {
+		const { id, data } = args;
+
+		const user = db.users.find(user => user.id === id);
+		if (!user) throw new Error('User not found');
+
+		if (typeof data.email === 'string') {
+			const emailTaken = db.users.some(user => user.email === data.email);
+
+			if (emailTaken) throw new Error('Email already in use');
+
+			user.email = data.email;
+		}
+		if (typeof data.name === 'string') user.name = data.name;
+
+		if (typeof data.age !== 'undefined') user.age = data.age;
+
+		return user;
+	},
+	createPost(parent, args, { db, pubsub }, info) {
+		const userExists = db.users.some(user => user.id === args.data.author);
+		if (!userExists) {
+			throw new Error('User not Found');
+		}
+		const post = {
+			id: uuidv4(),
+			...args.data
+		};
+		db.posts.push(post);
+		if (args.data.published) {
+			pubsub.publish('post', {
+				post: {
+					mutation: 'CREATED',
+					data: post
+				}
+			});
+		}
+		return post;
+	},
+
+	updatePost(parent, { id, data }, { db, pubsub }, info) {
+		const post = db.posts.find(post => post.id === id);
+		const originalPost = { ...post };
+		if (!post) throw new Error('Unable to find post');
+		if (typeof data.title === 'string') post.title = data.title;
+		if (typeof data.body === 'string') post.body = data.body;
+		if (typeof data.published === 'boolean') {
+			post.published = data.published;
+			if (originalPost.published && !post.published) {
+				pubsub.publish('post', {
+					post: {
+						mutation: 'DELETED',
+						data: originalPost
+					}
+				});
+			} else if (!originalPost.published && post.published) {
+				pubsub.publish('post', {
+					post: {
+						mutation: 'CREATED',
+						data: post
+					}
+				});
+			}
+		} else if (post.published) {
+			pubsub.publish('post', {
+				post: {
+					mutation: 'UPDATED',
+					data: post
+				}
+			});
+		}
+
+		return post;
+	},
+	deletePost(parent, args, { db, publish }, info) {
+		const postIndex = db.posts.findIndex(post => post.id === args.id);
+
+		if (postIndex === -1) throw new Error('Post not Found');
+		const [post] = db.posts.splice(postIndex, 1);
+		db.comments = db.comments.filter(comment => comment.post !== args.id);
+
+		if (post.published) {
+			pubsub.publish('post', {
+				mutation: 'DELETED',
+				data: post
+			});
+		}
+
+		return post;
+	},
+	createComment(parent, args, { db, pubsub }, info) {
+		const userExists = db.users.some(user => user.id === args.data.author);
+		const postExists = db.posts.some(
+			post => post.id === args.data.post && post.published
+		);
+		if (!userExists || !postExists) {
+			throw new Error('Unable to find user or post');
+		}
+		const comment = {
+			id: uuidv4(),
+			...args.data
+		};
+		db.comments.push(comment);
+		pubsub.publish(`comment ${args.data.post}`, {
+			comment: {
+				mutation: 'CREATED',
+				data: comment
+			}
+		});
+		return comment;
+	},
+	updateComment(parent, { id, data }, { db, pubsub }, info) {
+		const comment = db.comments.find(comment => comment.id === id);
+		if (!comment) throw new Error('Unable to find comment');
+		if (typeof data.text === 'string') comment.text = data.text;
+
+		pubsub;
+
+		return comment;
+	},
+	deleteComment(parent, args, { db, pubsub }, info) {
+		const commentIndex = db.comments.findIndex(
+			comment => comment.id === args.id
+		);
+		if (commentIndex === -1) throw new Error('Comment not found');
+		const [comment] = db.comments.splice(commentIndex, 1);
+		pubsub.publish(`comment ${comment.post}`, {
+			comment: {
+				mutation: 'UPDATED',
+				data: comment
+			}
+		});
+		return comment;
+	}
+};
+
+export { Mutation as default };
