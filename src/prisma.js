485a6e94cb76fa679b9933021bb0a06c2d9e7dd5
JonasMartins <jonas.w.martins@gmail.com>
2020-02-12 20:17:02 -0300

    auth secure secret string

--- src/prisma.js
+++ src/prisma.js
@@ -3,7 +3,7 @@ import { fragments } from './resolvers/index';
 const prisma = new Prisma({
 	typeDefs: 'src/generated/prisma.graphql',
 	endpoint: process.env.PRISMA_ENDPOINT,
-	secret: '7530C5B82764D1D46DC60F57B210EE41DCFEDBAF258E5DED33213281F36DC946',
+	secret: process.env.PRISMA_SECRET,
 	fragmentReplacements: fragments
 });
 export { prisma as default };
